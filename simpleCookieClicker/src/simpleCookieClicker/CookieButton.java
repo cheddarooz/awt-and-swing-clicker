a7e97488e42d77d18c757b70ceae844515fbd462
Abraham Lopez <43940695+cheddarooz@users.noreply.github.com>
2019-08-15 11:45:07 -0700

    Update CookieButton.java

--- simpleCookieClicker/src/simpleCookieClicker/CookieButton.java
+++ simpleCookieClicker/src/simpleCookieClicker/CookieButton.java
@@ -1,107 +1,188 @@
-package simpleCookieClicker;
-
-import java.awt.Button;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Rectangle;
+package cookieClicker;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.imageio.ImageIO;
+import javax.swing.*;
 
-public class CookieButton 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class CookieGUI 
 {
-	static int currentClicks;
-	static int frameXAxis = 500;
-	static int frameYAxis = 300;
-	static int grandmaWantsThisMany = 50;
+    static double currentClicks = 0;   // AKA CURRENT COOKIES 
+    
+    // rewards for clicks and parameters 
+    static double grandmaPrice = 5;
+    static long grandmaDelay = 2000L;
+    static long gmaDelayDecreaser = 100L;
+    static boolean gmaScheduleReset;
+    
+    static int futureCancel = 0;
+    
+    static int frameX = 1000;
+	static int frameY = 400;
 	
-	static Button incrementClicks  = new Button("Click me");
-	static Button resetClicks  = new Button("Reset Clicks");
-	static Button hireGrandma  = new Button("Hire Grandma");
+	static JFrame frame = new JFrame("Click Frame");
 	
-	static JFrame frame  = new JFrame("Cookie Clicker");
-	static JLabel label  = new JLabel();
-	static JLabel grandmasLabel  = new JLabel();
-	static JPanel panel = new JPanel();
+    static final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+    private static volatile ScheduledFuture<?> grandmasWorkshift;
+    private static Runnable grandma = new grandmasBakery();
+    
+    static JButton incrementClicks = new JButton ("Add Cookies");
+	static JButton resetClicks = new JButton("Reset Cookies");
+	static JButton grandmaPerk = new JButton("Grandma");
 	
-	static Color Hotpink = new Color(255, 105, 180);
 	
-	static Rectangle defaultRectangle = new Rectangle(200,40);
-	
+	 // add a label to the JPanel 
+    static JLabel labelCount = new JLabel();
+
 	public static void main(String[] args) 
 	{
-		panelAdjustments();
-		frameDimensions();
-		addToCount();
-		knowWhenClickIsClicked();
-
+		CreateFrame();
+		incrementClicks.addActionListener(new incrementClicksButtonListener());
+		grandmaPerk.addActionListener(new grandmaPerkButtonListener());
+		updateClicks(currentClicks);
 	}
-
-
-	private static void addToCount() 
+	
+	private static void CreateFrame()
 	{
-		label.setText("Clicked " + currentClicks + " times");
+				
+				setWindowProperties(frame, frameX, frameY);
+				// creates a JPanel to be displayed on the JFrame 
+				JPanel panel = new JPanel();
+				panel.setBackground(Color.PINK);
+				//add a panel to the JFrame
+				frame.getContentPane().add(panel); 
+				
+				labelCount.setPreferredSize(new Dimension(200, 200));
+				panel.add(labelCount);
+				updateClicks(currentClicks);
+				panel.add(resetClicks);
+				panel.add(incrementClicks); // add buttons to the JLabel
+				panel.add(grandmaPerk); //add button to the JLabel
+			//	grandmaPerk.setIcon(arg0);
+				resetClicks.addActionListener(new ActionListener()
+				{
+					@Override
+					public void actionPerformed(ActionEvent e)
+					{
+						currentClicks = 0;
+						updateClicks(currentClicks);
+						
+					}
+			    }
+		  );
+				
 	}
-
-	private static void panelAdjustments() 
-	{
-	    panel.setBackground(Hotpink);
-		frame.add(panel);
-		label.setLayout(null);
-		label.setBounds(defaultRectangle);
-	//    label.setPreferredSize(new Dimension(200,30));
-		panel.add(label);
-		panel.add(incrementClicks);
-		panel.add(resetClicks);
-	    
-		//grandmasLabel.setPreferredSize(new Dimension(400, 60));
-	    //frame.add(grandmasLabel);
-		//grandmasLabel.add(hireGrandma);
-
-	}
-
-	private static void frameDimensions() 
-	{
-
-		//frame.setSize(frameXAxis, frameYAxis);
-		frame.pack();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  
+	private static void setWindowProperties(JFrame frame, int frameX, int frameY)
+     {
+		frame.setSize(new Dimension(frameX, frameY));
 		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 		frame.setResizable(true);
-	}
+		//	cookieHouse.pack(); //this will pack the buttons together, the window will be smaller
+     }
+	
 
-	private static void knowWhenClickIsClicked()
+
+	  static class incrementClicksButtonListener implements ActionListener
 	{
-		incrementClicks.addActionListener(new ActionListener()
-			{
-				@Override
-				public void actionPerformed(ActionEvent e)
-				{
-					currentClicks++;
-					addToCount();
-				}
-				
-			}
-		);
-		resetClicks.addActionListener(new ActionListener()
-			{
-			   @Override
-			   public void actionPerformed(ActionEvent e)
-			   {
-				   currentClicks = 0;
-				   addToCount();
-			   }
+	 
+		@Override
+		public void actionPerformed(ActionEvent e) 
+		{
 			
-			}
-				
-		);
+			 currentClicks++;
+			 updateClicks(currentClicks);
+			
+		}
+		
 	}
-
+	  static class grandmaPerkButtonListener implements ActionListener
+	{
+			  @Override
+			  public void actionPerformed(ActionEvent e)
+			  {
+				  if(futureCancel == 2)
+				  {
+					  gmaRunnableService();
+				  }
+				  
+				  if(currentClicks >= grandmaPrice)
+				  {
+					  currentClicks = currentClicks - grandmaPrice;
+					  updateClicks(currentClicks);
+					  
+					  gmaRunnableService();
+					  
+					  grandmaPrice = Math.ceil(grandmaPrice * 1.5);
+					  grandmaDelay = grandmaDelay - 50;
+					  
+					  System.out.println("The current price for grandma is: " + grandmaPrice );
+					  System.out.println("The current delay for grandma is: " + grandmaDelay );
+					  System.out.println("The coordinates for grandmaButton is " + grandmaPerk.getX() + "," + grandmaPerk.getY() );
+			      }				  
+				  else if(currentClicks < grandmaPrice) 
+				  {
+					    currentClicks = currentClicks;
+				  }
+				  else
+				  {
+					  currentClicks = currentClicks;
+				  }
+				  
+			 }
+	}
+	  
+	private static void gmaRunnableService() // Grandma's runnable 
+	{
+	
+			if(futureCancel == 	0)
+			{
+				System.out.println("1st condition for grandma ");
+				grandmasWorkshift = executorService.scheduleWithFixedDelay(grandma, 2000, grandmaDelay, TimeUnit.MILLISECONDS);
+				futureCancel++; // is equal 1
+			}
+			else if(futureCancel == 1)
+			{
+				System.out.println("2nd condition for grandma ");
+				grandmasWorkshift.cancel(true);
+				grandmasWorkshift = executorService.scheduleWithFixedDelay(grandma, 2000, grandmaDelay, TimeUnit.MILLISECONDS);
+				futureCancel++;//is equal 2
+				System.out.println("futureCancel value is  " + futureCancel);
+			}
+			else 
+			{
+				System.out.println("resetting");
+				grandmasWorkshift.cancel(true);
+				futureCancel = 0;
+			}
+		
+	}
+	    
+	private static double updateClicks(double currentClicks) // refreshes cookies, should be called every time a change to the count is expected
+	{
+	  labelCount.setText("Cookies: "+ currentClicks);
+	  return currentClicks;
+	}
+	
+	private static final class grandmasBakery implements Runnable
+	{
+	 @Override public void run() 
+		{
+		 currentClicks++;
+		 updateClicks(currentClicks);
+		}
+	}
+		
+	  
 }
 
-
+	
 
